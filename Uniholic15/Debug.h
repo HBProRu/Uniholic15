bfddd0b33b76ba090ccbe0c39c02d6ce4e1ee8ab
HBPro <admin@hbpro.ru>
2015-09-04 23:23:09 +0300

    Add DebugMills

--- Uniholic15/Debug.h
+++ Uniholic15/Debug.h
@@ -1,15 +1,17 @@
-// Debug.h
-
 #ifdef DEBUG
+
+
+
 #define DebugInit(speed) Serial.begin(speed);
-#define Debug(message) {Serial.print(message);}
-#define Debugln(message) {Serial.println(message);}
+#define DebugMills() {Serial.print("["); Serial.print(TimeToString(millis())); Serial.print("] ");}
+#define DebugPrint(message) {Serial.print(message);}
+#define DebugPrintln(message) {Serial.println(message);}
 
 
 #else
 #define DebugInit(speed) {}
+#define DebugMills {}
+#define DebugPrint(message) {}
+#define DebugPrintln(message) {}
 
-#define Debug(message) {}
-#define Debugln(message) {}
-//
 #endif
