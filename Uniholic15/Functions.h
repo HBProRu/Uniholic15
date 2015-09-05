ab7045f37f6eae3262b713ac4ebc7cb8a3d52e94
HBPro <admin@hbpro.ru>
2015-09-05 07:25:36 +0300

    Change return format to "HH:mm:ss.SSS"

--- Uniholic15/Functions.h
+++ Uniholic15/Functions.h
@@ -1,12 +1,14 @@
 
-
+//¬озвращает врем€ в формате "HH:mm:ss.SSS" из миллисекунд
 char * TimeToString(unsigned long t)
 {
-	static char str[16];
+	static char str[14];
 	unsigned short ms = t % 1000;
 	unsigned char s = (t/1000) % 60;
 	unsigned char m = (t / 1000 / 60) % 60;
 	long h = (t / 1000 / 60 / 60);
-	sprintf(str, "%04ld:%02d:%02d.%03d", h, m, s, ms);
+	sprintf(str, "%02ld:%02d:%02d.%03d", h, m, s, ms);
 	return str;
 }
+
+
