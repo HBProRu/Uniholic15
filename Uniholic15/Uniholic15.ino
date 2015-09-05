1ed95a80175902135c379a742e677b1f28eb16d6
HBPro <admin@hbpro.ru>
2015-09-05 07:29:02 +0300

    Add function dbg_Recipe()

--- Uniholic15/Uniholic15.ino
+++ Uniholic15/Uniholic15.ino
@@ -4,25 +4,28 @@
 #include "OneWire.h"
 #include "DallasTemperature.h"
 #include "Recipe.h"
+#include "Functions.h"
+
 
 Recipe r1 = Recipe();
 
 void setup() {
 	
 	DebugInit(115200);
-	Serial.begin(115200);
+	DebugMills();
+	DebugPrintln("Starting Uniholic");
 
 	r1.name = "Hoegaarden";
 	r1.mashin = 45;
-	r1.step[0] = { 45, 10 };
-	r1.step[1] = { 60, 5 };
-	r1.step[2] = { 66, 30 };
-	r1.step[3] = { 72, 20 };
-	r1.step[4] = { 75, 20 };
+	r1.step[0] = STEP { 45, 10 };
+	r1.step[1] = STEP { 60, 5 };
+	r1.step[2] = STEP { 66, 30 };
+	r1.step[3] = STEP { 72, 20 };
+	r1.step[4] = STEP { 75, 20 };
 
-	r1.hop[0] = { 60 };
-	r1.hop[1] = { 10 };
-	r1.hop[2] = { 5 };
+	r1.hop[0] = HOP { 60 };
+	r1.hop[1] = HOP { 10 };
+	r1.hop[2] = HOP { 5 };
 
 	r1.boil = 90;
 
@@ -31,32 +34,36 @@ void setup() {
 
 void loop() {
 
-	Serial.print("Name: ");
-	Serial.println(r1.name);
-	Serial.print("MashIn: ");
-	Serial.println(r1.mashin);
-	Serial.println("Mashing:");
+	dbg_Recipe(r1);
+	delay(200);
+
+}
+
+void dbg_Recipe(Recipe recipe) {
+	DebugMills(); DebugPrintln("Start dbg_Recipe");
+	DebugPrint("Name: ");
+	DebugPrintln(r1.name);
+	DebugPrint("MashIn: ");
+	DebugPrintln(r1.mashin);
+	DebugPrintln("Mashing:");
 	for (byte i = 0; i < (sizeof(r1.step) / sizeof(STEP)); i++)
 	{
-		Serial.print("Step ");
-		Serial.print(i);
-		Serial.print(" - temp: ");
-		Serial.print(r1.step[i].temp);
-		Serial.print(" time: ");
-		Serial.println(r1.step[i].time);
+		DebugPrint("Step ");
+		DebugPrint(i);
+		DebugPrint(" - temp: ");
+		DebugPrint(r1.step[i].temp);
+		DebugPrint(" time: ");
+		DebugPrintln(r1.step[i].time);
 	}
-	Serial.println("Hops:");
+	DebugPrintln("Hops:");
 	for (byte i = 0; i < (sizeof(r1.hop) / sizeof(HOP)); i++)
 	{
-		Serial.print("Hop ");
-		Serial.print(i);
-		Serial.print(" time before boil: ");
-		Serial.println(r1.hop[i].time);
+		DebugPrint("Hop ");
+		DebugPrint(i);
+		DebugPrint(" time before boil: ");
+		DebugPrintln(r1.hop[i].time);
 	}
-	Serial.print("Boil: ");
-	Serial.println(r1.boil);
-	Serial.println("---------------------------------------");
-
-	delay(5000);
-//	Debug("sf", 2);
+	DebugPrint("Boil: ");
+	DebugPrintln(r1.boil);
+	DebugMills(); DebugPrintln("End dbg_Recipe");
 }
