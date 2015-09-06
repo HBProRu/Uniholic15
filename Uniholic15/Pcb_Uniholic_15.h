70bf422ed228fc2a0c9c193dca9a794e0736e207
HBPro <admin@hbpro.ru>
2015-09-06 16:19:24 +0300

    Fix define

--- Uniholic15/Pcb_Uniholic_15.h
+++ Uniholic15/Pcb_Uniholic_15.h
@@ -12,12 +12,12 @@
 #if LCD_TYPE_CONNECT == 1
 
 //экран RS, E, DB4, DB5, DB6, DB7
-#define OUTPUT_LCD_RS = 2
-#define OUTPUT_LCD_E = 3
-#define OUTPUT_LCD_DB4 = 4
-#define OUTPUT_LCD_DB5 = 5
-#define OUTPUT_LCD_DB6 = 6
-#define OUTPUT_LCD_DB7 = 7
+#define OUTPUT_LCD_RS 2
+#define OUTPUT_LCD_E 3
+#define OUTPUT_LCD_DB4 4
+#define OUTPUT_LCD_DB5 5
+#define OUTPUT_LCD_DB6 6
+#define OUTPUT_LCD_DB7 7
 
 #elif LCD_TYPE_CONNECT == 2 
 
@@ -27,13 +27,13 @@
 //Кол-во символов в строке
 #if LCD_CHARS_ROWS == 1
 
-#define I2C_LCD_CHARS = 16
-#define I2C_LCD_ROWS = 2
+#define I2C_LCD_CHARS 16
+#define I2C_LCD_ROWS 2
 
 #elif  LCD_CHARS_ROWS == 2
 
-#define I2C_LCD_CHARS = 20
-#define I2C_LCD_ROWS = 4
+#define I2C_LCD_CHARS 20
+#define I2C_LCD_ROWS 4
 
 #endif
 
@@ -42,10 +42,10 @@
 
 
 //Насос
-#define OUTPUT_PUMP = 8
+#define OUTPUT_PUMP 8
 
 //ТЭН 1
-#define OUTPUT_HEAT_1 = 9
+#define OUTPUT_HEAT_1 9
 
 //Динамик
-#define OUTPUT_BUZZER = 10
+#define OUTPUT_BUZZER 10
