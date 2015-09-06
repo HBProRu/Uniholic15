1a7bc733eaa4caf5f8b2cf97f1067a70c0ce151d
HBPro <admin@hbpro.ru>
2015-09-06 18:07:04 +0300

    Style Add BUZZER if

--- Uniholic15/Pcb_Uniholic_15.h
+++ Uniholic15/Pcb_Uniholic_15.h
@@ -11,31 +11,31 @@
 //LCD экран
 #if LCD_TYPE_CONNECT == 1
 
-//экран RS, E, DB4, DB5, DB6, DB7
-#define OUTPUT_LCD_RS 2
-#define OUTPUT_LCD_E 3
-#define OUTPUT_LCD_DB4 4
-#define OUTPUT_LCD_DB5 5
-#define OUTPUT_LCD_DB6 6
-#define OUTPUT_LCD_DB7 7
+	//экран RS, E, DB4, DB5, DB6, DB7
+	#define OUTPUT_LCD_RS 2
+	#define OUTPUT_LCD_E 3
+	#define OUTPUT_LCD_DB4 4
+	#define OUTPUT_LCD_DB5 5
+	#define OUTPUT_LCD_DB6 6
+	#define OUTPUT_LCD_DB7 7
 
 #elif LCD_TYPE_CONNECT == 2 
 
-//Адрес I2C
-#define I2C_LCD_ADDRESS 0x27
+	//Адрес I2C
+	#define I2C_LCD_ADDRESS 0x27
 
-//Кол-во символов в строке
-#if LCD_CHARS_ROWS == 1
+	//Кол-во символов в строке
+	#if LCD_CHARS_ROWS == 1
 
-#define I2C_LCD_CHARS 16
-#define I2C_LCD_ROWS 2
+		#define I2C_LCD_CHARS 16
+		#define I2C_LCD_ROWS 2
 
-#elif  LCD_CHARS_ROWS == 2
+	#elif  LCD_CHARS_ROWS == 2
 
-#define I2C_LCD_CHARS 20
-#define I2C_LCD_ROWS 4
+		#define I2C_LCD_CHARS 20
+		#define I2C_LCD_ROWS 4
 
-#endif
+	#endif
 
 #endif
 
@@ -48,4 +48,6 @@
 #define OUTPUT_HEAT_1 9
 
 //Динамик
-#define OUTPUT_BUZZER 10
+#if BUZZER != 0
+	#define OUTPUT_BUZZER 10
+#endif
